511cf0775ba22166e78feb85a2378f40f0cb95a2
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-04-18 16:34:26 +0700

    Update project

--- index.js
+++ index.js
@@ -2,21 +2,23 @@ import cv from 'opencv'
 import path from 'path'
 
 const fileName = path.resolve(__dirname, 'files', 'tux_profile.png')
-const outputFileName = path.resolve(__dirname, 'files', 'output.png')
+const outputFileName = path.resolve(__dirname, 'files', 'outputFix.png')
 
 cv.readImage(fileName, (err, im) => {
   // process before rotate
   const angle = 45
-  const [height, width] = im.size()
-  const [halfHeight, halfWidth] = [height / 2, width / 2]
+  const [h, w] = im.size()
+  const [cX, cY] = [w / 2, h / 2]
 
-  const rotMat = cv.Matrix.getRotationMatrix2D(-angle, halfWidth, halfHeight, 1.0)
-  const rotCos = Math.abs(rotMat.get(0, 0))
-  const rotSin = Math.abs(rotMat.get(0, 1))
-  const [newHeight, newWidth] = [(height * rotCos) + (width * rotSin)]
-  console.log(rotCos, rotSin)
+  const M = cv.Matrix.getRotationMatrix2D(angle, cX, cY, 1.0)
+  const cos = Math.abs(M.get(0, 0))
+  const sin = Math.abs(M.get(0, 1))
+  const nW = (h * sin) + (w * cos)
+  const nH = (h * cos) + (w * sin)
 
-  // complete custom image
-  im.rotate(0)
+  M.set(0, 2, M.get(0, 2) + (nW / 2) - cX)
+  M.set(1, 2, M.get(1, 2) + (nH / 2) - cY)
+
+  im.warpAffine(M, nW, nH)
   im.save(outputFileName)
 })
